cb99655337b19d6424012e1bcbdb40ae6769dbe7
Sohaib Elahi <164201408+sohaibelahigithub@users.noreply.github.com>
2024-10-11 23:44:59 +0000

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 13878
+let accountEmail = "sohaib.e0912003@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "sohaib.elaahi@gmail.com"
+accountPassword = "123456"
+accountCity = "Lahore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // Alert only works on browser, we are using nodejs for now
+
+let name = "Sohaib" // String Datatype
+let age = 22 // Int Datatype
+let isLoggedIn = false // Boolean Datatype
+let state = null // Null Datatype, Empty String
+
+// number => 2 to power 53
+// bigint => for large numbers
+// string => "" written in double quotes
+// boolean => True/False
+// Null => Standalone Value, Empty Value
+// Undefined => Variable is declared but not given any value
+// symbol => unique
+
+
+// object
+
+console.log(typeof "MyName") // Prints Type of given input , String
+
+
+console.log(typeof 13) // Prints Type of given input , Int
+
+
+console.log(typeof undefined) // Prints Type of given input , Undefined
+
+console.log(typeof null) // Prints Type of given input , Object
