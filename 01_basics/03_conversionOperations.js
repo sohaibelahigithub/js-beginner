f5daa9cf02fe2e5579655370bd50a38c91f4212f
Sohaib Elahi <164201408+sohaibelahigithub@users.noreply.github.com>
2024-10-12 09:23:59 +0000

    Conversion Operations 2

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -46,6 +46,50 @@ let aNumber = 3
 let stringNumber = String(aNumber)
 
 // 3 => "3" => String
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+// ************** Operations *****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);   // Add
+// console.log(2-2);   // Subtract
+// console.log(2*2);   // Multiply
+// console.log(2**2);  // Power
+// console.log(2/3);   // Divide
+// console.log(2%3);   // Remainder
+
+
+let str1 = "hello"
+let str2 = "Sohaib"
+// Concatenation
+let str3 = str1 + str2
+//console.log(str3);
+
+// ECMA Standards Primitive
+// console.log("1" + 2);       // 12
+// console.log(1 + "2");       // 12
+// console.log("1" + 2 + 2);   // 122   
+// console.log(1 + 2 + "2");   // 32
+
+// console.log((3+4) * 5 % 3); // BODMAS Rule, Answer => 2
+
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+ 2; // Inconsistent Code
+
+let gameCounter = 100;
+// Postfix
+gameCounter++; // Increment (+1) in gameCounter
+
+// Prefix
+++gameCounter; // Increment (+1) in gameCounter
+
+console.log(gameCounter);
