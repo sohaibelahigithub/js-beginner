e7c0e0705b9ade2f2c3db6d7e212f31321fe0efd
Sohaib Elahi <164201408+sohaibelahigithub@users.noreply.github.com>
2024-10-12 09:34:20 +0000

    Comparision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);     // Greater Than => True
+// console.log(2 >= 1);    // Greater Than Equals to => True
+// console.log(2 < 1);     // Less Than => False
+// console.log(2 == 1);    // Equals To => False
+// console.log(2 != 1);    // Not Equals To => True
+
+// console.log("2" > 1);    // true
+// console.log("02" > 1);   // true
+
+// console.log(null > 0);  // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true, converts null to a number => 0 then 
+//                         //compares and thats why it gives true, since 0 >= 0 => true
+
+
+// console.log(undefined == 0);    // false
+
+// ===
+console.log("2" == 2);
+console.log("2" === 2); // Tripe Equals === checks datatypes as well and compare datatype
+
+
