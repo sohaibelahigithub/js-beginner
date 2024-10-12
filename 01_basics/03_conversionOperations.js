a31b94717210419865aee27d4af814266d94ce99
Sohaib Elahi <164201408+sohaibelahigithub@users.noreply.github.com>
2024-10-12 08:56:42 +0000

    Conversion Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,51 @@
+let score = "24"
+let score_alpha = "24abc"
+let score_null = null
+let score_bool = true
+
+// console.log(typeof(score));
+// console.log(typeof(score_alpha));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+
+// let valueInNumber = Number(score_alpha)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// let valueInNumber = Number(score_null)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// let valueInNumber = Number(score_bool)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// Basic Conversions to Number
+// "24" => 24
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+let isLoggedIn = "Sohaib"
+
+// Empty String "" => Gives False
+// 1 => true; 0 => false
+// "Sohaib" => true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// String Conversion
+let aNumber = 3
+let stringNumber = String(aNumber)
+
+// 3 => "3" => String
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
