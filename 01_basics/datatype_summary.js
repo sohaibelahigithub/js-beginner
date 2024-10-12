7ce02a79d98d29059dbdddc720abc856d93b369d
Sohaib Elahi <164201408+sohaibelahigithub@users.noreply.github.com>
2024-10-12 10:28:36 +0000

    Datatype Summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,26 @@
+// Primitive
+
+// 7 Types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference Data Types (Non-Primitive)
+
+// Arrays, Objects, Functions
+
+// JavaScript is a dynamically typed language
+
+// Array:
+const heroes = ["Superman", "Batman", "Aquaman"];
+
+// Objects:
+let myObjs = {
+    name: "Sohaib",
+    age: 22
+}
+
+// Functions:
+
+const myfunction = function () {
+    console.log("Hello World");
+}
+
+console.log(typeof heroes); // Array Datatype is Object
